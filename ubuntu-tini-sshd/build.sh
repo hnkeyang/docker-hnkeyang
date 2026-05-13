58d3f18a5bbd5f13422344668e00136b3f9a0358
hnkeyang <hnkeyang@gmail.com>
2026-05-13 11:06:25 +0800

    ubuntu-ssh

--- ubuntu-tini-sshd/build.sh
+++ ubuntu-tini-sshd/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #
 docker build -t hnkeyang/ubuntu24-tini-sshd:1.0 -t hnkeyang/ubuntu24-tini-sshd:latest .
+#docker push hnkeyang/ubuntu24-tini-sshd:1.0 hnkeyang/ubuntu24-tini-sshd:latest

5d6244fc9a90109c987c6c49b3d7cfc98acfeb64
hnkeyang <hnkeyang@gmail.com>
2026-05-13 11:31:24 +0800

    base on hnkeyang/frr:10.6.1

--- endhost-frr/build.sh
+++ endhost-frr/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
-# 先从 https://github.com/FRRouting/frr/tree/master/docker/debian build frr docker
-docker build -t hnkeyang/endhost-frr:1.0.0 .
+# build hnkeyang/frr first
+docker build -t hnkeyang/endhost-frr:10.6.1 -t hnkeyang/endhost-frr:latest .
